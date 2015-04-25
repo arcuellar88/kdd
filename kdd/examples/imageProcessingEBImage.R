fc696a793120cb8e6f3862b8c3c6a1ba98bc48aa
Alejandro Rodriguez <alejrodr@ulb.ac.be>
2015-04-25 23:03:04 +0200

    Delete imageProcessingEBImage.R

--- kdd/examples/imageProcessingEBImage.R
+++
@@ -1,8 +0,0 @@
-#Load Library
-library("EBImage")
-#Path of the image
-Image <- readImage('/Users//alejandrorodriguez/Dropbox/Proyectos/IT4BI/UFRT/KDD/KDD Project/Archive/0/img-1.png')
-display(Image)
-print(Image)
-detach("package:EBImage")
-
