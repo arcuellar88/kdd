e29d0b64ac75fd2177eb7c0dbb08e05b08ba67ee
Truong Nguyen <truongnx15fl@gmail.com>
2015-06-08 18:46:38 +0200

    Add script to resize image locally

--- R/ImageResize.R
+++ R/ImageResize.R
@@ -26,24 +26,43 @@ resizeImageF = function(im, w.out, h.out) {
 #' @param x, height of the image
 #' @examples
 #' resizeImagesPageDB(4,30)
-resizeImagesPageDB = function(page, x) {
+resizeImagesPageDB = function(page, x , imgPath) {
   library(EBImage)
   library(RMySQL)
-  con <- dbConnect(MySQL(), user="root", password="", dbname="vesale", host="localhost")
+  con <- dbConnect(MySQL(), user="mysqluser", password="userul8mys9l", dbname="vesale", host="localhost")
+  #con <- dbConnect(MySQL(), user="root", password="", dbname="vesale", host="localhost")
   query=gsub("page1",page,"select url from vesale.image_index where page='page1'");
-  View(query)
+  #View(query)
   rs <- dbSendQuery(con,query)
   data <- fetch(rs, n=-1)
 
   for(i in 1:length(data[,]))
   {
-    im<-readImage(data[i,1])
+    
+    imgNameLength = as.integer(nchar(data[i,1]))
+    imageNamePath = substring(data[i,1], 3, imgNameLength)
+        
+    fullImagePath = paste(imgPath, imageNamePath , sep="/")
+    
+    im<-readImage(fullImagePath)
     imr<-resize(im,h=x)
     
-    writeImage(imr,data[i,1])
+    writeImage(imr, fullImagePath)
   }
   dbClearResult(rs)
   dbDisconnect(con)
+}
+
+# Resize all image in a folder
+resizeImagesLocal <- function(folderPath, height) {
+    library(EBImage)
+    images = list.files(folderPath)
+    for(image in images) {
+        fullImagePath = paste(folderPath, image, sep = "\\")
+        im <- readImage(fullImagePath)
+        imr <- resize(im, h = height)
+        writeImage(imr, fullImagePath)
+    }
 }
 
 #' Resize the patterns (images) of range of pages in a book
@@ -52,12 +71,13 @@ resizeImagesPageDB = function(page, x) {
 #' @param x, height of the image
 #' @examples
 #' resizeImagesPageDB(4,30)
-resizeImagesDB = function(startPage,endPage,x) {
+resizeImagesDB = function(startPage, endPage , x, imgPath) {
   #Set up working directory (make sure the thumbs directory is in there!)
-  setwd("/Users/alejandrorodriguez/Documents/DataMining/kdd/")
+  #setwd("D:/IT4BI/Semester 2/Knowledge Discovery and Data Mining/Project/kdd/")
   
   for(i in startPage:endPage)
   {
-    resizeImagesPageDB(i,x)
+    print(i)
+    resizeImagesPageDB(i,x, imgPath)
   }
 }
